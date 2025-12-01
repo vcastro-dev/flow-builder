abc765ef2cc52fc47522955fafcce974f7be2189
@vcastro-dev <vcastro2395@gmail.com>
2025-12-01 20:27:04 -0300

    chore: remove excessive logging

--- apps/api/src/modules/provision/infrastructure/providers/event-bus.ts
+++ apps/api/src/modules/provision/infrastructure/providers/event-bus.ts
@@ -9,14 +9,12 @@ export const createEventBus = () => {
   const subscribe = (eventName: string, handler: EventHandler<unknown>) => {
     const existingHandler = handlers.get(eventName) || [];
     handlers.set(eventName, [...existingHandler, handler]);
-    console.log(`[EVENT BUS] Subscribed to event: ${eventName}`);
   };
 
   const publish = async (event: any) => {
     const { name } = event;
     const eventHandlers = handlers.get(name) || [];
     await Promise.all(eventHandlers.map((handler) => handler(event)));
-    console.log(`[EVENT BUS] Published event: ${name}`);
   };
 
   return { subscribe, publish };
